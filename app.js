3e6e67eb1cf62b86d7be48f60b5f29746f91b4e9
Hieu Ngo <hieu.ngo12989@gmail.com>
2022-09-19 12:08:07 -0700

    Complete React Components with hover affects

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// TODO
+const { useState } = React;
+
+const TodoListItem = (props) => {
+  const [isDone, setIsDone] = useState(false);
+
+  let style = {
+    textDecoration: isDone ? 'line-through' : 'none'
+  };
+
+  return (
+      <li style={style} onClick={() => setIsDone(!isDone)}
+      onMouseOver={(e) => e.target.style.fontWeight = 'bold'}
+      onMouseOut={(e) => e.target.style.fontWeight = 'normal'}>
+        {props.todo}
+      </li>
+  );
+};
+
+const TodoList = (props) => (
+  <ul>
+    {props.todos.map((todo) => (
+      <TodoListItem todo={todo} />
+    ))}
+  </ul>
+)
+
+const App = () => (
+  <div>
+    <h1>My To-do List</h1>
+    <ul>Grocery List
+      <TodoList todos={['Rice', 'Meat', 'Veggies']} />
+    </ul>
+  </div>
+)
+
+ReactDOM.render(<App/>, document.getElementById("app"));
